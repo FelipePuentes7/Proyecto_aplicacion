9beb4154f32dbc129accc47285336fd6b0d5e53e
Felipe Puentes <fpuentesrivera@gmail.com>
2025-01-06 17:15:33 -0500

    interfaz de login y registro

--- index.php
+++
@@ -1,4 +0,0 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-Qui velit provident, magnam perspiciatis tenetur id esse illo 
-reiciendis temporibus porro accusamus excepturi ad. Officiis vero unde vel commodi cupiditate nisi!
-
