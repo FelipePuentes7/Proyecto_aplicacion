77a5da960ad41b7220c424c473e1d77c49a6d87e
Derek-fet <derek_quevedosa@fet.edu.co>
2025-01-29 21:11:42 -0500

    Borrador de ESTUDIANTES DE PROYECTOS

---
+++ views/general/historial_avanzes.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/historial_avanzes.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>MissionFet - Inicio Estudiante</title>
+</head>
+
+<body>
+
+  <nav class="navbar">
+    <!-- Espacio para el logo -->
+    <div class="logo">
+      <img src="/assets/img/logofet.png" alt="Logo FET" style="max-height: 50px;">
+    </div>
+
+    <!-- Links de navegación -->
+    <div class="nav-links">
+        <a href="#">Inicio</a>
+        <a href="#">Material de Apoyo</a>
+         <a href="#">Tutorías</a>
+    </div>
+
+    <!-- Acciones del usuario -->
+    <div class="user-actions">
+      <div class="notifications">
+        <i class="fa-solid fa-bell"></i> <!-- Ícono de notificaciones -->
+      </div>
+
+      <div class="dropdown">
+        <i class="fa-solid fa-user"></i> <!-- Ícono de perfil -->
+        <div class="dropdown-content">
+          a href="#">Editar Perfil</a>
+          <a href="#">Cerrar Sesión</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <div class="container_historial">
+      <div class="historial">
+        <h1>Historial de Avances del proyecto</h1>
+        <img src="">
+      </div>
+
+    <div class="container_tutorias">
+      
+      <div class="txt_agenda">
+        <div class="Tutoria_1">
+          <h1>TUTORIA 1</h1>
+          <h2>Titulo del avance</h2>
+          <p>Estado: Revisado</p>
+          <p>Fecha</p>
+        </div>
+
+        <div class="Tutoria_2">
+          <h1>TUTORIA 2</h1>
+          <h2>Titulo del avance</h2>
+          <p>Estado: Revisado</p>
+          <p>Fecha</p>
+        </div>
+
+        <div class="Tutoria_3">
+          <h1>TUTORIA 3</h1>
+          <h2>Titulo del avance</h2>
+          <p>Estado: En revision</p>
+          <p>Fecha</p>
+        </div>
+
+        <div class="Tutoria_4">
+          <h1>TUTORIA 4</h1>
+          <h2>Titulo del avance</h2>
+          <p>Estado: Rechazado</p>
+          <p>Fecha</p>
+        </div>
+
+      </div>
+
+    </div>
+      
+      <div class="panel_tutoria">
+        <h2>Titulo del avance</h2>
+        <p>Correciones del autor</p>
+       
+        <p>archivo Enviado:asd.pdf</p>
+        <p>Fecha Limite de Correcion: 15/08/2024</p>
+      </div>
+
+    </div>
+  </div>
+
+  <footer class="footer">
+
+    <!-- Sección de información -->
+     <div class="info-section">
+
+            <!-- Links -->
+            <div class="links">
+
+                <p class="title_footer"><strong>Fundación Escuela Tecnológica de Neiva</strong></p>
+
+                <a href="#">Inicio</a>
+                <a href="#">Material de Apoyo</a>
+            </div>
+
+            <!-- Información de contacto -->
+            <div class="contact">
+                <p>Email: soporte@universidad.edu</p>
+                <p>Dirección: Calle 123, Bogotá</p>
+                <p>Teléfono: +57 123 456 7890</p>
+            </div>
+
+            <!-- Espacio para la imagen -->
+            <div class="image">
+                <img src="/assets/img/image.png" alt="Imagen del footer" style="max-width: 50%;">
+            </div>
+        </div>
+
+        
+      <!-- Sección de redes sociales -->
+    <div class="social-section">
+            <!-- Redes sociales -->
+            <div class="social-links">
+                <a href="https://es-es.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://twitter.com/?lang=es" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                <!-- Logo del medio -->
+      <div class="logo">
+        <img src="/assets/img/logo_footer.png" alt="Footer FET" style="max-height: 50px;">
+      </div>
+              
+    </div>
+ 
+    </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/4fac1b523f.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ views/general/inicio_estudiantes.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/Inicio_Estu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>MissionFet - Inicio Estudiante</title>
+</head>
+
+<body>
+
+  <nav class="navbar">
+    <!-- Espacio para el logo -->
+    <div class="logo">
+      <img src="/assets/img/logofet.png" alt="Logo FET" style="max-height: 50px;">
+    </div>
+
+    <!-- Links de navegación -->
+    <div class="nav-links">
+        <a href="#">Inicio</a>
+        <a href="#">Material de Apoyo</a>
+         <a href="#">Tutorías</a>
+    </div>
+
+    <!-- Acciones del usuario -->
+    <div class="user-actions">
+      <div class="notifications">
+        <i class="fa-solid fa-bell"></i> <!-- Ícono de notificaciones -->
+      </div>
+
+      <div class="dropdown">
+        <i class="fa-solid fa-user"></i> <!-- Ícono de perfil -->
+        <div class="dropdown-content">
+          a href="#">Editar Perfil</a>
+          <a href="#">Cerrar Sesión</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <div class="container_inicio">
+      <div class="txt_inicio">
+        <h1>¡Bienvenido, [Nombre del Estudiante]!</h1>
+        <p>Organiza y gestiona tu proyecto de manera eficiente. Aquí encontrarás todo lo que necesitas para avanzar.</p>
+      </div>
+
+      <div class="img_inicio">
+        <img src="/assets/img/ejemplo.png">
+      </div>
+    </div>
+
+    <div class="container_calendario">
+      
+      <div class="txt_agenda">
+        <h2>Agenda</h2>
+        <p>Revisa tus próximas actividades y no pierdas de vista tus objetivos.</p>
+        <p>Tutor: pepe Rojas</p>
+        <p>Modalidad de la cita: Precencial</p>
+        <p>Lugar: Bloque 2 -202 o Meet.</p>
+      </div>
+
+      <div class="panel_agenda">
+        <img src="/assets/img/calendario.png">
+      </div>
+
+    </div>
+
+    <div class="container_agenda">
+      <div class="igm_proyecto">
+        <h1>Nombre del proyecto</h2>
+          <img src="/assets/img/agenda.jpg">
+        </div>
+      </div>
+      
+      <div class="panel_agenda">
+        <h2>Avance del proyecto: 50%</h2>
+        <p>Revisa tus próximas actividades y no pierdas de vista tus objetivos.</p>
+        <p>Fecha limite Proximo Avance: 20/11/2024</p>
+        <p>Tutor Asignado: Pepe rojas</p>
+        <p>Estado actual: Aprovado</p>
+      </div>
+      <div class="btn_avance">
+
+      <button>
+        Subir Nuevo Avance 
+      </button>
+
+      </div>
+  </div>
+
+  <footer class="footer">
+
+    <!-- Sección de información -->
+     <div class="info-section">
+
+            <!-- Links -->
+            <div class="links">
+
+                <p class="title_footer"><strong>Fundación Escuela Tecnológica de Neiva</strong></p>
+
+                <a href="#">Inicio</a>
+                <a href="#">Material de Apoyo</a>
+            </div>
+
+            <!-- Información de contacto -->
+            <div class="contact">
+                <p>Email: soporte@universidad.edu</p>
+                <p>Dirección: Calle 123, Bogotá</p>
+                <p>Teléfono: +57 123 456 7890</p>
+            </div>
+
+            <!-- Espacio para la imagen -->
+            <div class="image">
+                <img src="/assets/img/image.png" alt="Imagen del footer" style="max-width: 50%;">
+            </div>
+        </div>
+
+        
+      <!-- Sección de redes sociales -->
+    <div class="social-section">
+            <!-- Redes sociales -->
+            <div class="social-links">
+                <a href="https://es-es.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://twitter.com/?lang=es" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+                <a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
+                <!-- Logo del medio -->
+      <div class="logo">
+        <img src="/assets/img/logo_footer.png" alt="Footer FET" style="max-height: 50px;">
+      </div>
+              
+    </div>
+ 
+    </div>
+    </footer>
+
+    <script src="https://kit.fontawesome.com/4fac1b523f.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
