a5fe13b08dd78c9fb83fd5d11b8a3bde5a8ad1cc
JDMENDEZ777 <jesusdavidmendezmendez@gmail.com>
2025-04-18 20:54:01 -0500

    INCONVENIENTE PROYECTOS

--- views/estudiantes/Inicio_Estudiante.php
+++ views/estudiantes/Inicio_Estudiante.php
@@ -37,7 +37,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/Inicio_Estu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>MissionFet - Perfil Estudiante</title>
+    <title>MissionFet - Inicio Estudiante</title>
 </head>
 
 <body>
